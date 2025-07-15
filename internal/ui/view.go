0932be1d53ce100316970df236d97d4c0eb83a23
Amir Raminfar <findamir@gmail.com>
2025-07-15 08:24:26 -0700

    fixes open

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -3,8 +3,17 @@ package ui
 import "github.com/charmbracelet/lipgloss"
 
 func (m model) View() string {
+	keymap := m.keyMap
+	rows := m.table.Rows()
+	if len(rows) > 0 && keymap.Open.Enabled() {
+		selected := rows[m.table.Cursor()]
+		if selected.container.Dozzle == "" {
+			keymap.Open.SetEnabled(false)
+		}
+	}
+
 	return lipgloss.JoinVertical(
 		lipgloss.Left, m.table.View(),
-		lipgloss.PlaceHorizontal(m.width, lipgloss.Center, helpBarStyle.Render(m.help.View(m.keyMap))),
+		lipgloss.PlaceHorizontal(m.width, lipgloss.Center, helpBarStyle.Render(m.help.View(keymap))),
 	)
 }
