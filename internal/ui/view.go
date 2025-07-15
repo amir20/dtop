e4b9dc10c7ddb7cd0766de8eca2548d1f1c00e5e
Amir Raminfar <findamir@gmail.com>
2025-07-15 10:12:47 -0700

    fix: fixes bug when nothing is selected

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -5,7 +5,7 @@ import "github.com/charmbracelet/lipgloss"
 func (m model) View() string {
 	keymap := m.keyMap
 	rows := m.table.Rows()
-	if len(rows) > 0 && keymap.Open.Enabled() {
+	if len(rows) > 0 && keymap.Open.Enabled() && m.table.Cursor() != -1 {
 		selected := rows[m.table.Cursor()]
 		if selected.container.Dozzle == "" {
 			keymap.Open.SetEnabled(false)
