80ce06f00624c506aabf4984babb7bbf09687d5a
Amir Raminfar <findamir@gmail.com>
2025-11-03 09:15:39 -0800

    fix: fixes self update

--- src/update.rs
+++ src/update.rs
@@ -5,12 +5,17 @@ use std::error::Error;
 pub fn run_update() -> Result<(), Box<dyn Error>> {
     println!("Checking for updates...");
 
+    // Determine the target triple for this platform
+    let target = self_update::get_target();
+
     let status = self_update::backends::github::Update::configure()
         .repo_owner("amir20")
         .repo_name("dtop")
         .bin_name("dtop")
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
+        .target(&target)
+        .bin_path_in_archive("{{ bin }}-{{ target }}/{{ bin }}")
         .build()?
         .update()?;
 
